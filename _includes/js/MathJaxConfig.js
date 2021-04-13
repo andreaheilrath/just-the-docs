018b1fa473b41ec22ad66892d548f98e71024b35
Andrea Heilrath <49402693+andreaheilrath@users.noreply.github.com>
2021-04-13 10:06:35 +0200

    Update MathJaxConfig.js

--- _includes/js/MathJaxConfig.js
+++ _includes/js/MathJaxConfig.js
@@ -3,3 +3,5 @@ MathJax.Hub.Config({
     inlineMath: [ ['$','$'], ['\\(','\\)'] ]
   }
 });
+
+MathJax.Ajax.loadComplete("https://raw.githubusercontent.com/andreaheilrath/just-the-docs/master/_includes/js/MathJaxConfig.js")
