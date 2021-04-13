104cd67a991df0dd4122a318a76bbdaea29fdcb4
Andrea Heilrath <49402693+andreaheilrath@users.noreply.github.com>
2021-04-13 11:50:13 +0200

    Delete MathJaxConfig.js

--- _includes/js/MathJaxConfig.js
+++
@@ -1,7 +0,0 @@
-MathJax.Hub.Config({
-  tex2jax: {
-    inlineMath: [ ['$','$'], ['\\(','\\)'] ]
-  }
-});
-
-MathJax.Ajax.loadComplete("https://raw.githubusercontent.com/andreaheilrath/just-the-docs/master/_includes/js/MathJaxConfig.js")
